fde236ec7cd940878062e5884105a05ae62409ef
Alex Duller <alexduller@gmail.com>
2009-10-20 18:54:25 +0100

    Have a simple grid which the camera can navigate

--- src/coursework.cpp
+++ src/coursework.cpp
@@ -1,3 +1,7 @@
+/**
+ * CM0304 coursework 2009
+ * Copyright 2009 Alex Duller
+ */
 #include <iostream>
 #include <math.h>
 
@@ -7,182 +11,105 @@
 #include <GL/glut.h>
 #endif /* __APPLE__ */
 
-float angle=0.0;
-float x=0.0f,y=1.75f,z=5.0f;
-float lx=0.0f,ly=0.0f,lz=-1.0f;
-float ratio=1.0;
-int frame,mytime,timebase=0;
-char s[30];
+// The y rotation of the camera in degrees
+float angle = 0;
 
-GLuint DLid;
+// Rate at which camera is rotated, higher means faster rotation
+float rotate_sensitivity = 0.4;
 
-GLuint createDL(void);
+float dist_sensitivity = 0.1;
 
-void changeSize(int w, int h)	{
+// The inverted position of the camera
+float position[3]={0, -2, 0};
 
-  // Prevent a divide by zero, when window is too short
-  // (you cant make a window of zero width).
-  if(h == 0)
-    h = 1;
+// Used to keep track of keyboard keys - this is so we can use multiple keys
+// at once.
+int g_keys[128] = {false};
 
-  ratio = 1.0f * w / h;
-  // Reset the coordinate system before modifying
-  glMatrixMode(GL_PROJECTION);
+void special_callback(int key, int, int)
+{
+  g_keys[key] = true;
+}
+
+void special_up_callback(int key, int, int)
+{
+  g_keys[key] = false;
+}
+
+void display_callback(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT);
   glLoadIdentity();
-	
-  // Set the viewport to be the entire window
-  glViewport(0, 0, w, h);
 
-  // Set the clipping volume
-  gluPerspective(45,ratio,1,1000);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  gluLookAt(x, y, z, 
-            x + lx,y + ly,z + lz,
-            0.0f,1.0f,0.0f);
-}
+  // Camera position
+  glRotatef(angle, 0, 1, 0);
+  glTranslatef(position[0], position[1], position[2]);
 
-void initScene() {
-
-  glEnable(GL_DEPTH_TEST);
-  DLid = createDL();
-}
-
-
-void drawSnowMan() {
-
-
-  glColor3f(1.0f, 1.0f, 1.0f);
-
-  // Draw Body	
-  glTranslatef(0.0f ,0.75f, 0.0f);
-  glutSolidSphere(0.75f,20,20);
-
-
-  // Draw Head
-  glTranslatef(0.0f, 1.0f, 0.0f);
-  glutSolidSphere(0.25f,20,20);
-
-  // Draw Eyes
-  glPushMatrix();
-  glColor3f(0.0f,0.0f,0.0f);
-  glTranslatef(0.05f, 0.10f, 0.18f);
-  glutSolidSphere(0.05f,10,10);
-  glTranslatef(-0.1f, 0.0f, 0.0f);
-  glutSolidSphere(0.05f,10,10);
-  glPopMatrix();
-
-  // Draw Nose
-  glColor3f(1.0f, 0.5f , 0.5f);
-  glRotatef(0.0f,1.0f, 0.0f, 0.0f);
-  glutSolidCone(0.08f,0.5f,10,2);
-}
-
-
-
-GLuint createDL() {
-  GLuint snowManDL,loopDL;
-
-  snowManDL = glGenLists(1);
-  loopDL = glGenLists(1);
-
-  glNewList(snowManDL,GL_COMPILE);
-  drawSnowMan();
-  glEndList();
-
-  glNewList(loopDL,GL_COMPILE);
-  for(int i = -3; i < 3; i++)
-    for(int j=-3; j < 3; j++) {
-      glPushMatrix();
-      glTranslatef(i*10.0,0,j * 10.0);
-      glCallList(snowManDL);
-      glPopMatrix();
-    }
-  glEndList();
-
-  return(loopDL);
-}
-
-
-void renderScene(void) {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  // Draw ground
-
-  glColor3f(0.9f, 0.9f, 0.9f);
-  glBegin(GL_QUADS);
-  glVertex3f(-100.0f, 0.0f, -100.0f);
-  glVertex3f(-100.0f, 0.0f,  100.0f);
-  glVertex3f( 100.0f, 0.0f,  100.0f);
-  glVertex3f( 100.0f, 0.0f, -100.0f);
+  // Draw a grid
+  glBegin(GL_LINES);
+  for (int i = -30; i <= 30; ++i)
+  {
+    glVertex3f(i, 0, -30);
+    glVertex3f(i, 0, 30);
+    glVertex3f(30, 0, i);
+    glVertex3f(-30, 0, i);
+  }
   glEnd();
 
-  // Draw 36 SnowMen
-
-  glCallList(DLid);
-  frame++;
-  mytime=glutGet(GLUT_ELAPSED_TIME);
-  if (mytime - timebase > 1000) {
-    sprintf(s,"FPS:%4.2f",frame*1000.0/(mytime-timebase));
-    glutSetWindowTitle(s);
-    timebase = mytime;		
-    frame = 0;
-  }
   glutSwapBuffers();
 }
 
-void orientMe(float ang) {
+void reshape_callback(int w, int h)
+{
+  if (w == 0)
+    h = 1;
 
-
-  lx = sin(ang);
-  lz = -cos(ang);
+  glViewport(0, 0, w, h);
+  glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluLookAt(x, y, z, 
-            x + lx,y + ly,z + lz,
-            0.0f,1.0f,0.0f);
+  gluPerspective(45, (float)w/h, 0.1, 100);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
 }
 
+void idle_callback()
+{
+  if (g_keys[GLUT_KEY_LEFT])
+    angle -= rotate_sensitivity;
 
-void moveMeFlat(int i) {
-  x = x + i*(lx)*0.3;
-  z = z + i*(lz)*0.3;
+  if (g_keys[GLUT_KEY_RIGHT])
+    angle += rotate_sensitivity;
 
-  glLoadIdentity();
-  gluLookAt(x, y, z, 
-            x + lx,y + ly,z + lz,
-            0.0f,1.0f,0.0f);
-}
-
-
-void inputKey(unsigned char c, int x, int y) {
-
-  switch (c) {
-    case 'a' : angle -= 0.03f;orientMe(angle);break;
-    case 's' : angle +=0.03f;orientMe(angle);break;
-    case 't' : moveMeFlat(1);break;
-    case 'g' : moveMeFlat(-1);break;
+  if (g_keys[GLUT_KEY_UP])
+  {
+    position[0] += -sin(angle * M_PI / 180.0f) * dist_sensitivity;
+    position[2] += cos(angle * M_PI / 180.0f) * dist_sensitivity;
   }
+
+  if (g_keys[GLUT_KEY_DOWN])
+  {
+    position[0] -= -sin(angle * M_PI / 180.0f) * dist_sensitivity;
+    position[2] -= cos(angle * M_PI / 180.0f) * dist_sensitivity;
+  }
+
+  glutPostRedisplay();
 }
 
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
   glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-  glutInitWindowPosition(100,100);
-  glutInitWindowSize(300,300);
-  glutCreateWindow("SnowMen");
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize(800, 600);
+  glutInitWindowPosition(100, 100);
+  glutCreateWindow("Grid");
 
-  initScene();
-
-  glutKeyboardFunc(inputKey);
-
-  glutDisplayFunc(renderScene);
-  glutIdleFunc(renderScene);
-
-  glutReshapeFunc(changeSize);
+  glutDisplayFunc(display_callback);
+  glutReshapeFunc(reshape_callback);
+  glutSpecialFunc(special_callback);
+  glutSpecialUpFunc(special_up_callback);
+  glutIdleFunc(idle_callback);
 
   glutMainLoop();
 
-  return(0);
+  return 0;
 }
