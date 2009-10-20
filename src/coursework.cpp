f75a8b18ca1dbdc329e2efef821777cf89ceac05
Alex Duller <alexduller@gmail.com>
2009-10-20 15:17:16 +0100

    Added autoconf checks for OpenGL

--- src/coursework.cpp
+++ src/coursework.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+#ifndef __apple__
+#define __apple__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif /* __apple__ */
+
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-  
+  glutInit(&argc, argv);
+  glutMainLoop();
 }
