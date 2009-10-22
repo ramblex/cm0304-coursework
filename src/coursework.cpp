ad023452ef0b2137d65372fa3432b5693e38212f
Alex Duller <alexduller@gmail.com>
2009-10-22 16:27:28 +0100

    Added parametric curve. Still need to add normals

--- src/coursework.cpp
+++ src/coursework.cpp
@@ -1,5 +1,10 @@
 /**
+ * @file coursework.cpp
  * CM0304 coursework 2009
+ *
+ * Implements a basic 3D OpenGL scene which can be navigated in a walk-through
+ * fashion.
+ *
  * Copyright 2009 Alex Duller
  */
 #include <iostream>
@@ -11,7 +16,7 @@
 #include <GL/glut.h>
 #endif /* __APPLE__ */
 
-// Conversion factors for degrees to radians
+// Conversion factors for degrees to radians (from wireframe.cc)
 const double deg_to_rad (M_PI / 180.0);
 const double rad_to_deg (180.0 / M_PI);
 
@@ -33,21 +38,35 @@ float position[3]={0, -2, -30};
 // at once.
 int g_keys[128] = {false};
 
+// Handle special keys when they are pressed (idea from ...)
 void special_callback(int key, int, int)
 {
   g_keys[key] = true;
 }
 
+// Unset the special key flag.
 void special_up_callback(int key, int, int)
 {
   g_keys[key] = false;
 }
 
-void teapot()
+/**
+ * Q2. 3D objects
+ * A parametric surface which is not a plane, sphere, cylinder or cone. This
+ * can be added to the scene through the scene() function.
+ *
+ * A parametric surface is defined by parametric functions:
+ * x = fx(u, v)
+ * y = fy(u, v)
+ * y = fz(u, v)
+ *
+ * @param accuracy The approximation accuracy of the surface by polygons
+ */
+void cylinder()
 {
-  // Teapot material
-  static GLfloat diffuse[] = {0.0, 0.7, 0.0, 1.0};
-  static GLfloat ambient[] = {0.0, 0.5, 0.0, 1.0};
+  // Surface material
+  static GLfloat diffuse[] = {0.7, 0.0, 0.0, 1.0};
+  static GLfloat ambient[] = {0.5, 0.0, 0.0, 1.0};
   static GLfloat specular[] = {1.0, 1.0, 1.0, 1.0};
   static GLfloat shine (127.0);
 
@@ -57,25 +76,45 @@ void teapot()
   glMaterialfv (GL_FRONT, GL_SPECULAR, specular);
   glMaterialfv (GL_FRONT, GL_SHININESS, &shine);
 
-  // Push current modelview matrix on a matrix stack to save current
-  // transformation.
-  glPushMatrix ();
+  glPushMatrix();
+  glTranslated(0.0, 1.0, 0.0);
 
-  // Multiply modelview matrix with additional transformation to move
-  // and rotate the teapot to a different position.
-  glTranslated (5.0, 1.0, 0.0);
-  glRotated (0.0, 1.0, 0.0, 0.0);
-  
-  // Glut function for drawing a solid teapot
-  glutSolidTeapot (1.0);
+  int r = 1;
 
-  // Get original matrix back from stack (undo above transformation
-  // for objects drawn after this one)
-  glPopMatrix ();
+  for (double h = 0; h < 5; h += 0.1)
+  {
+    glBegin(GL_QUAD_STRIP);
+    for (double theta = -180; theta <= 180; theta += 0.1)
+    {
+      double rad_theta = deg_to_rad * theta;
+      double x = r * cos(rad_theta);
+      double y = r * sin(rad_theta);
+      double z = h;
+      glVertex3f(x, z, y);
 
+      rad_theta = deg_to_rad * (theta + 0.1);
+       x = r * cos(rad_theta);
+       y = r * sin(rad_theta);
+       z = h + 0.1;
+      glVertex3f(x, z, y);
+    }
+    glEnd();
+  }
+
+  glPopMatrix();
   return;
 }
 
+/**
+ * Q2. 3D objects
+ * Stanford bunny mesh
+ */
+void bunny()
+{
+
+}
+
+// Sphere from shaded.cc
 void sphere()
 {
   // Angle steps for facet size
@@ -101,27 +140,28 @@ void sphere()
   // surface position (note that this is a very special case!).
   //
   // We first set the normal of the vertex and then the vertex itself.
-  
+
   // Quadrilateral strips
   for (double phi = -90.0 + da; phi < 90.0; phi += da) {
     glBegin (GL_QUAD_STRIP);
     for (double thet = -180.0; thet <= 180.0; thet += da) {
-      glNormal3d (sin (deg_to_rad * thet) * cos (deg_to_rad * phi),
+      double u = sin (deg_to_rad * thet) * cos (deg_to_rad * phi);
+      glNormal3d (u,
 		  cos (deg_to_rad * thet) * cos (deg_to_rad * phi),
-		  sin (deg_to_rad * phi)); 
+		  sin (deg_to_rad * phi));
       glVertex3d (sin (deg_to_rad * thet) * cos (deg_to_rad * phi),
 		  cos (deg_to_rad * thet) * cos (deg_to_rad * phi),
-		  sin (deg_to_rad * phi)); 
+		  sin (deg_to_rad * phi));
       glNormal3d (sin (deg_to_rad * thet) * cos (deg_to_rad * (phi + da)),
 		  cos (deg_to_rad * thet) * cos (deg_to_rad * (phi + da)),
-		  sin (deg_to_rad * (phi + da)));  
+		  sin (deg_to_rad * (phi + da)));
       glVertex3d (sin (deg_to_rad * thet) * cos (deg_to_rad * (phi + da)),
 		  cos (deg_to_rad * thet) * cos (deg_to_rad * (phi + da)),
-		  sin (deg_to_rad * (phi + da)));  
+		  sin (deg_to_rad * (phi + da)));
     }
     glEnd();
   }
-  
+
   // North pole
   glBegin (GL_TRIANGLE_FAN);
   glNormal3d (0.0, 0.0, 1.0);
@@ -154,6 +194,7 @@ void sphere()
   return;
 }
 
+// Draw the floor of the scene
 void floor()
 {
   static GLfloat diffuse[] = {0.7, 0.7, 0.0, 1.0};
@@ -171,11 +212,9 @@ void floor()
   // transformation.
   glPushMatrix ();
 
-  // Multiply modelview matrix with additional transformation to move
-  // and rotate the teapot to a different position.
   glTranslated (0.0, 0.0, 0.0);
   glRotated (0.0, 1.0, 0.0, 0.0);
-  
+
   glBegin(GL_QUADS);
     glVertex3f(-30, 0, -30);
     glVertex3f(-30, 0, 30);
@@ -188,18 +227,71 @@ void floor()
   glPopMatrix ();
 }
 
-// Add objects to the scene
+static double sec(double x)
+{
+  return (1 / cos(x));
+}
+
+void parametric_surface(double res)
+{
+  static GLfloat diffuse[] = {0.1, 0.9, 0.0, 1.0};
+  static GLfloat ambient[] = {0.5, 0.5, 0.0, 1.0};
+  static GLfloat specular[] = {1.0, 1.0, 1.0, 1.0};
+  static GLfloat shine (3.0);
+
+  // Set material
+  glMaterialfv (GL_FRONT, GL_AMBIENT, ambient);
+  glMaterialfv (GL_FRONT, GL_DIFFUSE, diffuse);
+  glMaterialfv (GL_FRONT, GL_SPECULAR, specular);
+  glMaterialfv (GL_FRONT, GL_SHININESS, &shine);
+
+  // Push current modelview matrix on a matrix stack to save current
+  // transformation.
+  glPushMatrix ();
+
+  glTranslated (0.0, -0.9, 0.0);
+  glRotated (0.0, 1.0, 0.0, 0.0);
+
+  for (double s = -180/4; s <= 180/4; s += res)
+  {
+    glBegin(GL_QUAD_STRIP);
+    for (double t = -180/4; t <= 180/4; t += res)
+    {
+      // The parametric equations for the hyperbolic paraboloid are from:
+      // http://www.physicsforums.com/showthread.php?t=89217
+      double u = deg_to_rad*s;
+      double v = deg_to_rad*t;
+      glVertex3f(2 * tan(v) * sec(u),
+                 sqrt(2) * sec(u) * sec(v),
+                 3 * tan(u));
+
+      u = deg_to_rad * (s + res);
+      v = deg_to_rad * (t + res);
+      glVertex3f(2 * tan(v) * sec(u),
+                 sqrt(2) * sec(u) * sec(v),
+                 3 * tan(u));
+    }
+    glEnd();
+  }
+
+  // Get original matrix back from stack (undo above transformation
+  // for objects drawn after this one)
+  glPopMatrix ();
+}
+
+// Add objects to the scene. Order does not matter since we are using
+// DEPTH_TEST
 void scene()
 {
   floor();
-  sphere();
-  teapot();
+  parametric_surface(0.3);
+  // sphere();
 
   return;
 }
 
 /**
- * Initialise the lights for the scene
+ * Initialise the lights for the scene. Adapted from shaded.cc
  */
 void init_lights()
 {
@@ -216,7 +308,7 @@ void init_lights()
   // Use Gourad-shading technique
   glShadeModel(GL_SMOOTH);
 
-  { 
+  {
     // Light source 0
     static GLfloat ambient[] = {0.5f, 0.5f, 0.5f, 1.0f};
     static GLfloat diffuse[] = {0.8f, 0.8f, 0.8f, 1.0f};
@@ -229,11 +321,12 @@ void init_lights()
     glLightfv (GL_LIGHT0, GL_DIFFUSE, diffuse);
     glLightfv (GL_LIGHT0, GL_SPECULAR, specular);
   }
+
   return;
 }
 
 // Set position of light sources
-void lights() 
+void lights()
 {
   static GLfloat light0_pos[] = {0.0f, 3.0f, 2.0f, 1.0f};
   glLightfv(GL_LIGHT0, GL_POSITION, light0_pos);
@@ -241,6 +334,7 @@ void lights()
   return;
 }
 
+// Render the scene
 void display_callback(void)
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -250,15 +344,19 @@ void display_callback(void)
   glRotatef(angle, 0, 1, 0);
   glTranslatef(position[0], position[1], position[2]);
 
+  // Add light to the scene
   lights();
 
+  // Render the scene
   glCallList(scene_dl);
 
   glFlush();
 
+  // Switch to back buffer since we are using double buffering
   glutSwapBuffers();
 }
 
+// Called when window is resized (from ...)
 void reshape_callback(int w, int h)
 {
   if (w == 0)
@@ -272,6 +370,10 @@ void reshape_callback(int w, int h)
   glLoadIdentity();
 }
 
+/**
+ * Handle keyboard input. Idea adapted from ()
+ * Q1 (b) Navigation system
+ */
 void idle_callback()
 {
   if (g_keys[GLUT_KEY_LEFT])
@@ -292,6 +394,7 @@ void idle_callback()
     position[2] -= cos(angle * M_PI / 180.0f) * dist_sensitivity;
   }
 
+  // Q1. (a) continuously render scene
   glutPostRedisplay();
 }
 
@@ -308,14 +411,17 @@ int main(int argc, char *argv[])
   glClearDepth(1.0);
   glEnable(GL_DEPTH_TEST);
 
+  // Setup callback functions
   glutDisplayFunc(display_callback);
   glutReshapeFunc(reshape_callback);
   glutSpecialFunc(special_callback);
   glutSpecialUpFunc(special_up_callback);
   glutIdleFunc(idle_callback);
 
+  // Initialise lights so that they are ready to be added to the scene
   init_lights();
 
+  // Create a display list for the scene
   scene_dl = glGenLists(1);
   glNewList(scene_dl, GL_COMPILE);
   scene();
